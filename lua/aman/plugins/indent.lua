3a3316dda0ccabdc4457fbc53d7acc9903f4333f
Aman-Meenia <amanmeenia09876@gmail.com>
2024-08-14 01:52:00 +0530

    Add indentation

---
+++ lua/aman/plugins/indent.lua
@@ -0,0 +1,10 @@
+-- To show indentation lines
+
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	main = "ibl",
+	opts = {
+		indent = { char = "┊" },
+	},
+}
