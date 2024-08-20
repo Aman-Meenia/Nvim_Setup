46d091d7c9669418ef9c4212896261f4b991eda3
Aman-Meenia <amanmeenia09876@gmail.com>
2024-08-21 00:47:47 +0530

    Command to run c++ code (space B)

--- lua/aman/core/keymaps.lua
+++ lua/aman/core/keymaps.lua
@@ -71,3 +71,49 @@ vim.keymap.set("n", "<leader>o", "<cmd>lua require('goto-preview').goto_preview_
 -- LazyDocker setup
 
 vim.keymap.set("n", "<leader>k", "<cmd>LazyDocker<CR>", { desc = "Toggle LazyDocker", noremap = true, silent = true })
+
+-- vim.api.nvim_set_keymap("n", "<F5>", ":w<CR>:!g++ -o %:r %<CR>:!./%:r<CR>", { noremap = true, silent = false })
+
+-- Existing configuration (abbreviated for brevity)
+-- ... [Previous configuration remains unchanged]
+
+-- LazyDocker setup
+-- vim.keymap.set("n", "<leader>k", "<cmd>LazyDocker<CR>", { desc = "Toggle LazyDocker", noremap = true, silent = true })
+
+-- Function to compile and run C++ program in a new Wezterm window
+local function compile_and_run_cpp()
+	-- Save the current file
+	vim.cmd("write")
+
+	-- Get the full path of the current file and its directory
+	local file_path = vim.fn.expand("%:p")
+	local file_dir = vim.fn.expand("%:p:h")
+	local file_name = vim.fn.expand("%:t:r")
+
+	-- Compile the C++ file
+	local compile_command = string.format('g++ -o "%s/%s" "%s"', file_dir, file_name, file_path)
+	local compile_output = vim.fn.system(compile_command)
+
+	-- Check if compilation was successful
+	if vim.v.shell_error ~= 0 then
+		-- If compilation failed, display the error in a split window
+		vim.cmd("split")
+		local lines = vim.split(compile_output, "\n")
+		vim.api.nvim_buf_set_lines(0, -1, -1, false, lines)
+		vim.cmd("normal! G")
+		print("Compilation failed. See error message above.")
+	else
+		-- If compilation succeeded, run the program in a new Wezterm window
+		local run_command = string.format("./%s", file_name)
+		local wezterm_command = string.format(
+			'wezterm start --cwd "%s" -- sh -c "%s; echo; echo Press any key to close...; read -n 1"',
+			file_dir,
+			run_command
+		)
+		vim.fn.system(wezterm_command)
+		print("Program running in a new Wezterm window.")
+	end
+end
+
+-- Map F5 to the compile_and_run_cpp function
+vim.keymap.set("n", "<leader>b", compile_and_run_cpp, { desc = "Compile and Run C++ in Wezterm", noremap = true })
