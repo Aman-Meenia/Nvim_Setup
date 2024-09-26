82b17f4afb475e3a93a090e33657f548420f2b5f
Aman-Meenia <amanmeenia09876@gmail.com>
2024-09-26 13:25:10 +0530

    Add the border to cmp completions

--- lua/aman/plugins/nvim-cmp.lua
+++ lua/aman/plugins/nvim-cmp.lua
@@ -91,6 +91,8 @@ return {
 		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
 		require("luasnip.loaders.from_vscode").lazy_load()
 
+		-- Snippets are added to specific filetypes
+
 		cmp.setup({
 			completion = {
 				completeopt = "menu,menuone,preview,noselect",
@@ -99,6 +101,10 @@ return {
 				expand = function(args)
 					luasnip.lsp_expand(args.body)
 				end,
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
 			},
 			mapping = cmp.mapping.preset.insert({
 				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
