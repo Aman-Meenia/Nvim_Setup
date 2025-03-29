5473665269150a90dd2a735826181c6e0e4eeee2
Aman-Meenia <amanmeenia09876@gmail.com>
2025-03-29 14:48:11 +0530

    Add Undofile option and chage color of current foucs link and on copy something

--- lua/aman/core/options.lua
+++ lua/aman/core/options.lua
@@ -1,6 +1,8 @@
 vim.cmd("let g:netrw_liststyle = 3")
 local opt = vim.opt -- for conciseness
 
+opt.undofile = true
+opt.undodir = vim.fn.expand("~/.undodir")
 -- Set maximum line length indicator
 opt.colorcolumn = "100" -- Shows a vertical line at column 100
 
@@ -16,6 +18,10 @@ opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
 opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
+opt.smartindent = true
+opt.breakindent = true
+
+vim.opt.confirm = true -- confirm to save changes before exiting modified buffer
 
 -- line wrapping
 opt.wrap = false -- disable line wrapping
@@ -23,9 +29,14 @@ opt.wrap = false -- disable line wrapping
 -- search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+opt.hlsearch = true
+opt.incsearch = true
 
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
+opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
+opt.cursorlineopt = "number"
+vim.cmd([[highlight LineNr guifg=#FFA500]]) -- Orange for all relative line numbers
 
 -- appearance
 
@@ -47,3 +58,22 @@ opt.splitbelow = true -- split horizontal window to the bottom
 
 -- turn off swapfile
 opt.swapfile = false
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "*",
+	callback = function()
+		vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#91594C", bold = true })
+	end,
+})
+
+local function augroup(name)
+	return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+-- -- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = augroup("highlight_yank"),
+	callback = function()
+		(vim.hl or vim.highlight).on_yank()
+	end,
+})
